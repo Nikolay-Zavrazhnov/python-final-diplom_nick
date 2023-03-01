cb2a7099b36ca06500c450b0525970f17f36b8fc
nikolay <niko3avr@yandex.ru>
2023-03-01 16:21:54 +0400

    add selery#1

---
+++ orders/orders/celery.py
@@ -0,0 +1,10 @@
+
+import os
+from celery import Celery
+
+'''Интеграция  в celery'''
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orders.settings")
+app = Celery("orders") #создаем экземпляр класса нашего django-приложения
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
