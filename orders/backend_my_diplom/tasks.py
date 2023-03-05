91849645d7f2eea166581451f9d434340322811e
nikolay <niko3avr@yandex.ru>
2023-03-05 14:02:19 +0400

    add celery task step1

--- orders/backend_my_diplom/tasks.py
+++ orders/backend_my_diplom/tasks.py
@@ -1,3 +1,4 @@
+
 from django.conf import settings
 from celery import shared_task
 from django.core.mail import EmailMultiAlternatives
--- orders/orders/celery.py
+++ orders/orders/celery.py
@@ -2,7 +2,7 @@
 import os
 from celery import Celery
 
-# Интеграция  в celery
+# Интеграция в celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orders.settings")
 app = Celery("orders") #создаем экземпляр класса нашего django-приложения
